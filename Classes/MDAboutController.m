fd762aa887baa9eeebfa441cd6918adaec5fab33
Dimitri Bouniol <dimitribouniol@mochidev.com>
2011-04-20 17:09:35 -0700

    added all orientations

--- Classes/MDAboutController.m
+++ Classes/MDAboutController.m
@@ -150,7 +150,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 @end
