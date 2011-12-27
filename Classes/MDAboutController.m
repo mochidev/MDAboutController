3856d624d820b0494ef9e2b23f5070260249ab7a
Dimitri Bouniol <dimitribouniol@mochidev.com>
2011-12-27 10:11:03 -0800

    modified license

--- Classes/MDAboutController.m
+++ Classes/MDAboutController.m
@@ -6,17 +6,22 @@
 //  Copyright 2011 Mochi Development Inc. All rights reserved.
 //
 //  Copyright (c) 2010 Dimitri Bouniol, Mochi Development, Inc.
+//  
+//  Copyright (c) 2011 Dimitri Bouniol, Mochi Development, Inc.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
+//  of this software, associated artwork, and documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
+//  
+//  1. The above copyright notice and this permission notice shall be included in
+//     all copies or substantial portions of the Software.
+//  2. Neither the name of Mochi Development, Inc. nor the names of its
+//     contributors or products may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//  
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,7 +29,10 @@
 //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
-//
+//  
+//  EleMints, the EleMints Icon, Mochi Dev, and the Mochi Development logo are
+//  copyright Mochi Development, Inc.
+//  
 //  Also, it'd be super awesome if you left in the credit line generated
 //  automatically by the code that links back to this page :)
 //
